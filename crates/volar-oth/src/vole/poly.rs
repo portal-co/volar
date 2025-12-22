e48bdd526b91917057dc56c4a1439d577a44e9d1
Graham Kelly <gkgoat6700@gmail.com>
2025-12-22 16:58:17 -0500

    add polys

--- crates/volar-oth/src/vole/poly.rs
+++ crates/volar-oth/src/vole/poly.rs
@@ -4,6 +4,30 @@ pub struct Poly<N: ArrayLength<T>, T> {
     pub c1: GenericArray<T, N>,
 }
 impl<N: ArrayLength<T>, T> Poly<N, T> {
+    pub fn get_qs<
+        Q: Clone + Mul<T, Output: Mul<Q, Output = A> + Into<A>>,
+        A: Add<A, Output = A>,
+        M: ArrayLength<Q> + ArrayLength<A>,
+    >(
+        &self,
+        root: GenericArray<Q, M>,
+        inputs: [GenericArray<GenericArray<Q, M>, N>; 2],
+    ) -> GenericArray<A, M>
+    where
+        N: ArrayLength<GenericArray<Q, M>>,
+        T: Clone,
+    {
+        GenericArray::generate(|i| {
+            let mut sum: A = (root[i].clone() * self.c0.clone()).into();
+            for j in 0..N::to_usize() {
+                let a = inputs[0][j][i].clone();
+                let b = self.c1[j].clone();
+                let c = inputs[1][j][i].clone();
+                sum = sum + a * b * c;
+            }
+            sum
+        })
+    }
     pub fn apply<M, O: Mul<O, Output = O> + Add<O, Output = O> + Default + Clone>(
         &self,
         voles: [GenericArray<Vole<M, T>, N>; 2],
