3c42b423364efb0daeb51fb7bc788740a0edbb43
Graham Kelly <gkgoat6700@gmail.com>
2025-12-22 18:19:32 -0500

    field backends and 64-bit fields

--- crates/volar-oth/src/field.rs
+++ crates/volar-oth/src/field.rs
@@ -1,5 +1,6 @@
 use super::*;
-use core::ops::{Add, BitXor, Mul, Sub};
+use core::ops::{Add, BitAnd, BitXor, BitXorAssign, Mul, Shl, ShlAssign, ShrAssign, Sub};
+pub mod backend;
 macro_rules! u8_field {
     ($($a:ident),*) => {
         $(
@@ -16,23 +17,58 @@ macro_rules! u8_field {
         )*
     };
 }
-u8_field!(Galois, Bytes);
-impl Add<Bytes> for Bytes {
-    type Output = Bytes;
-    fn add(self, rhs: Bytes) -> Self::Output {
-        Bytes(self.0.wrapping_add(rhs.0))
+macro_rules! u64_field {
+    ($($a:ident),*) => {
+        $(
+            #[repr(transparent)]
+            #[derive(Clone,Copy, Debug, PartialEq, Eq,PartialOrd, Ord, Hash,Default)]
+            pub struct $a(pub u64);
+            impl BitXor<u8> for $a{
+                type Output = Self;
+                fn bitxor(self, rhs: u8) -> Self::Output {
+                    $a(u64::from_ne_bytes(self.0.to_ne_bytes().map(|b| b ^ rhs)))
+                }
+            }
+
+        )*
+    };
+}
+u8_field!(Galois, BitsInBytes);
+u64_field!(Galois64, BitsInBytes64);
+impl Add<BitsInBytes> for BitsInBytes {
+    type Output = BitsInBytes;
+    fn add(self, rhs: BitsInBytes) -> Self::Output {
+        BitsInBytes(self.0 ^ rhs.0)
     }
 }
-impl Mul<Bytes> for Bytes {
-    type Output = Bytes;
-    fn mul(self, rhs: Bytes) -> Self::Output {
-        Bytes(self.0.wrapping_mul(rhs.0))
+impl Mul<BitsInBytes> for BitsInBytes {
+    type Output = BitsInBytes;
+    fn mul(self, rhs: BitsInBytes) -> Self::Output {
+        BitsInBytes(self.0 & rhs.0)
     }
 }
-impl Sub<Bytes> for Bytes {
-    type Output = Bytes;
-    fn sub(self, rhs: Bytes) -> Self::Output {
-        Bytes(self.0.wrapping_sub(rhs.0))
+impl Sub<BitsInBytes> for BitsInBytes {
+    type Output = BitsInBytes;
+    fn sub(self, rhs: BitsInBytes) -> Self::Output {
+        BitsInBytes(self.0 ^ rhs.0)
+    }
+}
+impl Add<BitsInBytes64> for BitsInBytes64 {
+    type Output = BitsInBytes64;
+    fn add(self, rhs: BitsInBytes64) -> Self::Output {
+        BitsInBytes64(self.0 ^ rhs.0)
+    }
+}
+impl Mul<BitsInBytes64> for BitsInBytes64 {
+    type Output = BitsInBytes64;
+    fn mul(self, rhs: BitsInBytes64) -> Self::Output {
+        BitsInBytes64(self.0 & rhs.0)
+    }
+}
+impl Sub<BitsInBytes64> for BitsInBytes64 {
+    type Output = BitsInBytes64;
+    fn sub(self, rhs: BitsInBytes64) -> Self::Output {
+        BitsInBytes64(self.0 ^ rhs.0)
     }
 }
 impl Add<Galois> for Galois {
@@ -41,23 +77,14 @@ impl Add<Galois> for Galois {
         Galois(self.0 ^ rhs.0)
     }
 }
+
 impl Mul<Galois> for Galois {
     type Output = Galois;
     fn mul(self, rhs: Galois) -> Self::Output {
         let mut a = self.0;
         let mut b = rhs.0;
-        let mut p = 0u8;
-        for _ in 0..8 {
-            if (b & 1) != 0 {
-                p ^= a;
-            }
-            let high_bit = a & 0x80;
-            a <<= 1;
-            if high_bit != 0 {
-                a ^= 0x1b; // x^8 + x^4 + x^3 + x + 1
-            }
-            b >>= 1;
-        }
+        let c = 0x1b; // x^8 + x^4 + x^3 + x + 1
+        let p = backend::field_mul(a, b, c);
         Galois(p)
     }
 }
@@ -65,5 +92,27 @@ impl Sub<Galois> for Galois {
     type Output = Galois;
     fn sub(self, rhs: Galois) -> Self::Output {
         Galois(self.0 ^ rhs.0)
+    }
+}
+impl Add<Galois64> for Galois64 {
+    type Output = Galois64;
+    fn add(self, rhs: Galois64) -> Self::Output {
+        Galois64(self.0 ^ rhs.0)
+    }
+}
+impl Mul<Galois64> for Galois64 {
+    type Output = Galois64;
+    fn mul(self, rhs: Galois64) -> Self::Output {
+        let mut a = self.0;
+        let mut b = rhs.0;
+        let c = 0x1b; // x^8 + x^4 + x^3 + x + 1
+        let p = backend::field_mul(a, b, c);
+        Galois64(p)
+    }
+}
+impl Sub<Galois64> for Galois64 {
+    type Output = Galois64;
+    fn sub(self, rhs: Galois64) -> Self::Output {
+        Galois64(self.0 ^ rhs.0)
     }
 }
