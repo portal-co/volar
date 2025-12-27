b3c25ededc86ef2d56f78dc9336a5973ea83f8f7
Graham Kelly <gkgoat6700@gmail.com>
2025-12-27 18:11:05 -0500

    fix abo generation, use hash-based commitments again

--- crates/volar-dyn/src/spec_dyn/byte_gen.rs
+++ crates/volar-dyn/src/spec_dyn/byte_gen.rs
@@ -4,6 +4,11 @@ use super::*;
 use alloc::vec::Vec;
 use core::ops::Deref;
 
+use digest::Digest;
+use cipher::BlockEncrypt;
+
+use volar_common::hash_commitment::CommitmentCore;
+
 use super::vole::VopeDyn;
 
 /// Create a dynamic `VopeDyn` from a slice of byte-like inputs, mirroring
@@ -41,22 +46,59 @@ pub struct ABODyn {
     pub per_byte: Vec<Vec<u8>>,
 }
 
+/// Lightweight trait for block-cipher-like types usable by the dynamic
+/// reimplementation. This mirrors the role of `BlockEncrypt + From<[u8;32]>`
+/// used in the `volar-spec` crate but is sized for dynamic buffers.
+pub trait ByteBlockEncryptDyn {
+    fn from_key(key: [u8; 32]) -> Self;
+    fn encrypt_block(&mut self, block: &mut [u8]);
+}
+
 /// Dynamic version of `double` from `byte_gen.rs`.
-pub fn double_dyn(
-    a: &[u8],
-    mut encrypt: impl FnMut([u8; 32], &mut [u8]),
-) -> [Vec<u8>; 2] {
+pub fn double_dyn<B: ByteBlockEncryptDyn>(a: &[u8]) -> [Vec<u8>; 2] {
     let block_size = a.len();
-    let mut out0 = vec![0u8; block_size];
-    let mut out1 = vec![0u8; block_size];
+    let mut out0 = a.to_vec();
+    let mut out1 = a.to_vec();
 
-    let key0 = [0u8; 32];
-    encrypt(key0, &mut out0);
+    let mut b0 = B::from_key([0u8; 32]);
+    b0.encrypt_block(&mut out0);
 
-    let key1 = [1u8; 32];
-    encrypt(key1, &mut out1);
+    let mut b1 = B::from_key([1u8; 32]);
+    b1.encrypt_block(&mut out1);
 
     [out0, out1]
+}
+
+/// Dynamic equivalent of `ByteBlockEncrypt::gen_abo` from `volar-spec`.
+pub fn gen_abo_dyn<B: ByteBlockEncryptDyn, D: Digest>(
+    a: &[u8],
+    rand: &impl AsRef<[u8]>,
+    k: usize,
+) -> ABODyn {
+    let mut h = D::new();
+    let mut per_byte: Vec<Vec<u8>> = Vec::with_capacity(k);
+
+    // compute number of bits needed to index `k` entries
+    let bits = (usize::BITS as usize) - (k.leading_zeros() as usize);
+
+    for i in 0..k {
+        let mut acc = a.to_vec();
+        for b in 0..bits {
+            let doubled = double_dyn::<B>(&acc);
+            if ((i >> b) & 1) != 0 {
+                acc = doubled[1].clone();
+            } else {
+                acc = doubled[0].clone();
+            }
+        }
+        let core = acc;
+        let c = CommitmentCore::<D>::commit(&core, rand);
+        h.update(c.as_ref());
+        per_byte.push(core);
+    }
+
+    let commit = h.finalize();
+    ABODyn { commit: commit.as_slice().to_vec(), per_byte }
 }
 
 /// Expanded variant that applies a function `f` to each input before folding.
