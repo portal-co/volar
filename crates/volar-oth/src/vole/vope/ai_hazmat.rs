f3c1b1106c84820f25610677233b4bbf1cf6b3ef
Graham Kelly <gkgoat6700@gmail.com>
2025-12-26 19:22:37 -0500

    multiplication with only `clone`

--- crates/volar-oth/src/vole/vope/ai_hazmat.rs
+++ crates/volar-oth/src/vole/vope/ai_hazmat.rs
@@ -1,16 +1,13 @@
 use super::*;
-impl<N, T, K> Vope<N, T, K> 
-where 
-    N: VoleArray<T>, 
-    T: Add<Output = T> + Mul<Output = T> + Default + Copy, // T is the extension field (e.g. GF(2^128))
-    K: ArrayLength<GenericArray<T, N>>
+impl<N, T, K> Vope<N, T, K>
+where
+    N: VoleArray<T>,
+    T: Add<Output = T> + Mul<Output = T> + Default + Clone, // T is the extension field (e.g. GF(2^128))
+    K: ArrayLength<GenericArray<T, N>>,
 {
     /// Multiplies this Vope with another, returning a Vope of degree K1 + K2.
     /// This represents the algebraic product in the context of Delta.
-    pub fn mul_generalized<K2, KRes>(
-        &self, 
-        other: &Vope<N, T, K2>
-    ) -> Vope<N, T, KRes>
+    pub fn mul_generalized<K2, KRes>(&self, other: &Vope<N, T, K2>) -> Vope<N, T, KRes>
     where
         K2: ArrayLength<GenericArray<T, N>>,
         KRes: ArrayLength<GenericArray<T, N>>, // KRes must be K + K2
@@ -19,15 +16,9 @@ where
         let mut res_u = GenericArray::<GenericArray<T, N>, KRes>::default();
         let mut res_v = GenericArray::<T, N>::default();
 
-
-
         macro_rules! get_coeff {
             ($v:expr, $u:expr, $idx:expr) => {
-                if $idx == 0 {
-                    $v
-                } else {
-                    &$u[$idx - 1]
-                }
+                if $idx == 0 { $v } else { &$u[$idx - 1] }
             };
         }
 
@@ -42,12 +33,14 @@ where
                 if k == 0 {
                     // Resulting offset (degree 0)
                     for lane in 0..N::to_usize() {
-                        res_v[lane] = res_v[lane] + a_coeff[lane] * b_coeff[lane];
+                        res_v[lane] =
+                            res_v[lane].clone() + a_coeff[lane].clone() * b_coeff[lane].clone();
                     }
                 } else {
                     // Resulting higher degree coefficients
                     for lane in 0..N::to_usize() {
-                        res_u[k - 1][lane] = res_u[k - 1][lane] + a_coeff[lane] * b_coeff[lane];
+                        res_u[k - 1][lane] = res_u[k - 1][lane].clone()
+                            + a_coeff[lane].clone() * b_coeff[lane].clone();
                     }
                 }
             }
