4db30ffff59a114ad0cdf3c0a7cd5a4736a635d0
Graham Kelly <gkgoat6700@gmail.com>
2025-12-28 00:37:34 -0500

    add ir

---
+++ crates/volar-ir/src/lib.rs
@@ -0,0 +1,68 @@
+#![no_std]
+
+use alloc::{collections::btree_map::BTreeMap, vec::Vec};
+extern crate alloc;
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
+pub struct IrBlockId(pub usize);
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
+pub struct IRBlocks(pub Vec<IRBlock>);
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
+pub struct IRBlock {
+    pub params: Vec<IRTypeId>,
+    pub stmts: Vec<IRStmt>,
+    pub terminator: IRTerminator,
+}
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
+pub struct IRTypeId(pub usize);
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
+pub struct IRTypes(pub Vec<IRType>);
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
+pub enum IRType {
+    Bit,
+    Vec(usize, IRTypeId),
+    Tuple(Vec<IRTypeId>),
+    Galois8AES,
+    Galois64,
+    Block { params: Vec<IRTypeId> },
+}
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
+pub struct IRVarId(pub usize);
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
+pub enum IRStmt {
+    StorageRead {
+        ty: IRTypeId,
+        addr: IRVarId,
+    },
+    StorageWrite {
+        src: IRVarId,
+        ty: IRTypeId,
+        addr: IRVarId,
+    },
+    Const(Vec<u8>, IRTypeId),
+    Linear(BTreeMap<IRVarId, u8>),
+    Transmute {
+        src: IRVarId,
+        src_ty: IRTypeId,
+        dst_ty: IRTypeId,
+    },
+    Poly {
+        coeffs: Vec<(IRVarId, u8)>,
+        constant: Vec<u8>,
+    },
+    Rol {
+        src: IRVarId,
+        ty: IRTypeId,
+        n: usize,
+    },
+    Ror {
+        src: IRVarId,
+        ty: IRTypeId,
+        n: usize,
+    },
+}
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
+pub enum IRTerminator {
+    Return { src: IRVarId, ty: IRTypeId },
+    Jmp { func: IrBlockId, args: Vec<IRVarId> },
+    JmpDyn { func: IRVarId, args: Vec<IRVarId> },
+}
