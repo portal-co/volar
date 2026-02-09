7ffca5c3ec5550c3c6652090672485eece747403
gkgoat <65402989+gkgoat1@users.noreply.github.com>
2026-02-09 03:09:33 -0500

    bool ir go brr

--- crates/volar-ir/src/boolar.rs
+++ crates/volar-ir/src/boolar.rs
@@ -1,5 +1,7 @@
 use super::{ir::*, *};
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
+pub struct BIrBlocks(pub Vec<BIrBlock>);
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
 pub struct BIrBlock {
     pub params: u32,
     pub stmts: Vec<BIrStmt>,
