906ebac41f392cc1647a288babf0a6638be944a9
gkgoat <65402989+gkgoat1@users.noreply.github.com>
2026-03-29 22:51:20 -0400

    add vaffle

--- crates/compiler/volar-compiler-common/src/lib.rs
+++ crates/compiler/volar-compiler-common/src/lib.rs
@@ -1,13 +1,47 @@
 #![no_std]
+
+use core::error::Error;
 extern crate alloc;
-#[derive(Clone, Copy,PartialEq, Eq, PartialOrd, Ord,Debug)]
-pub enum Import{
-    Action{
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub enum Import {
+    Action {
         num_inputs: usize,
-        num_outputs: usize
+        num_outputs: usize,
     },
-    Oracle{
+    Oracle {
         num_inputs: usize,
-        num_outputs: usize
+        num_outputs: usize,
+    },
+}
+pub trait Stitcher<Target, Context> {
+    type Output;
+    type Error: Error;
+    fn stitch(&mut self, target: Target, context: Context) -> Result<Self::Output, Self::Error>;
+}
+pub trait Transferrable<S, Context> {
+    type Output;
+    type NewContext;
+    type Error: Error;
+    fn transfer(
+        self,
+        stitcher: &mut S,
+        context: Context,
+    ) -> Result<(Self::Output, Self::NewContext), Self::Error>;
+}
+impl<S, Context, T: Transferrable<S, Context>, U: Transferrable<S, T::NewContext, Error = T::Error>>
+    Transferrable<S, Context> for (T, U)
+{
+    type Output = (T::Output, U::Output);
+    type NewContext = U::NewContext;
+    type Error = T::Error;
+    fn transfer(
+        self,
+        stitcher: &mut S,
+        context: Context,
+    ) -> Result<(Self::Output, Self::NewContext), Self::Error> {
+        let (a, b) = self;
+        let (a_out, a_ctx) = a.transfer(stitcher, context)?;
+        let (b_out, b_ctx) = b.transfer(stitcher, a_ctx)?;
+        Ok(((a_out, b_out), b_ctx))
     }
 }
--- crates/ir/vaffle/src/lib.rs
+++ crates/ir/vaffle/src/lib.rs
@@ -1,3 +1,74 @@
 #![no_std]
 
+use alloc::{collections::btree_map::BTreeMap, string::String, vec::Vec};
+
 extern crate alloc;
+pub struct Module {
+    pub funcs: Vec<FuncDecl>,
+    pub sigs: Vec<SigDecl>,
+    pub exports: BTreeMap<String, FuncId>,
+}
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub struct SigId(pub usize);
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub struct FuncId(pub usize);
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub struct BlockId(pub usize);
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub struct ValueId(pub usize);
+pub struct SigDecl {
+    pub params: Vec<Type>,
+    pub results: Vec<Type>,
+}
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
+#[non_exhaustive]
+pub enum Type {
+    _32,
+    _64,
+    _16,
+    _8,
+    AES8,
+}
+pub enum FuncDecl {
+    Import {
+        module: String,
+        name: String,
+        sig: SigId,
+    },
+    Body(FuncBody),
+}
+pub struct FuncBody {
+    pub sig: SigId,
+    pub blocks: Vec<Block>,
+    pub values: Vec<Value>,
+    pub entry: BlockId,
+}
+pub struct Block {
+    pub params: Vec<(ValueId, Type)>,
+    pub stmts: Vec<ValueId>,
+    pub terminator: Terminator,
+}
+pub struct Target {
+    pub block: BlockId,
+    pub args: Vec<ValueId>,
+}
+pub enum Terminator {
+    Return { values: Vec<ValueId> },
+    Jump(Target),
+    ReturnCall { func: FuncId, args: Vec<ValueId> },
+}
+pub enum Value {
+    Param {
+        block: BlockId,
+        ty: Type,
+        idx: usize,
+    },
+    Call {
+        func: FuncId,
+        args: Vec<ValueId>,
+    },
+    Output {
+        value: ValueId,
+        idx: usize,
+    },
+}
