f6c4cf66c926cfc46fa6b3b4a823b70b6476a982
gkgoat <65402989+gkgoat1@users.noreply.github.com>
2026-06-20 14:25:27 -0400

    tracing 1

--- crates/spec/volar-common/src/lib.rs
+++ crates/spec/volar-common/src/lib.rs
@@ -5,3 +5,20 @@ pub use hybrid_array::{Array, ArraySize};
 
 pub mod hash_commitment;
 pub mod length_doubling;
+
+pub trait Tracer<Ct, Idx> {
+    type Payload;
+    fn tick(self, payload: Option<Self::Payload>) -> TracerResult<Self, Ct, Idx>
+    where
+        Self: Sized;
+}
+pub enum TracerResult<Tracer, Ct, Idx> {
+    ResumeWithCt {
+        tracer: Tracer,
+        ct: Ct,
+    },
+    Finished {
+        idx: Idx,
+        r#continue: Option<Tracer>,
+    },
+}
